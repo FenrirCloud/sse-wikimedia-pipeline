6df56dc4797605341116b3a28d90af5498a3444b
fenrirDMS <raad.shoman@gmail.com>
2025-08-21 10:51:15 +0300

    minor bug fixes

--- producer/create_minio_bucket.py
+++ producer/create_minio_bucket.py
@@ -2,7 +2,7 @@ from minio import Minio
 import os
 
 client = Minio(
-    os.getenv("MINIO_ENDPOINT", "localhost:9000"),
+    os.getenv("MINIO_ENDPOINT", "minio:9000"),
     access_key=os.getenv("MINIO_ACCESS_KEY", "minioadmin"),
     secret_key=os.getenv("MINIO_SECRET_KEY", "minioadmin"),
     secure=False
